bd246defe7b96020413fc17ef9573b58d883561e
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-09 15:30:49 -0500

    Register form new version

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -30,28 +30,20 @@ const login = async () => {
 </script>
 
 <template>
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
-                class="mx-auto h-10 w-auto"
+                class="mx-auto h-20 w-auto"
                 src="../assets/img/small-logo-image.png"
                 alt="Lexi AI"
             />
-            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                Log in to your account
+            <h2 class="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                Log into your account
             </h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form @submit.prevent="login" class="space-y-6">
+            <form @submit.prevent="login" class="space-y-4">
                 <div>
                     <label for="email" class="block text-sm/6 font-medium text-gray-900"
                         >Email address</label
@@ -100,7 +92,7 @@ const login = async () => {
 
             <p class="mt-10 text-center text-sm/6 text-gray-500">
                 Don't have account, 
-                <RouterLink to="/register" class="font-semibold text-blue-600 hover:text-blue-500"
+                <RouterLink to="/register" class="ms-3 font-semibold text-blue-600 hover:text-blue-500"
                     >Register</RouterLink>
             </p>
         </div>
