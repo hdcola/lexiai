134877289cd0c6c31fd26a90faaefd8468eee95f
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-08 22:51:35 -0500

    added Login and Navbar

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { mdiMenu } from '@mdi/js'
+import logo from '@/assets/img/logo-horizontal.png'
+import { useRoute } from 'vue-router'
+
+const isLoggedIn = ref<boolean>(!!localStorage.getItem('accessToken'));
+
+const isActiveLink = (routePath: string) => { 
+  const route = useRoute();
+  return route.path === routePath;
+} 
+
+const logout = () => {
+  localStorage.removeItem('accessToken');
+  window.location.reload();
+  console.log('User logged out');
+};
+
+
+// State for toggling the menu
+const isMenuOpen = ref<boolean>(false)
+</script>
+
+<template>
+  <nav class="bg-blue-500 border-b border-blue-500">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        
+        <!-- Logo -->
+        <RouterLink to="/" class="flex items-center">
+          <img class="h-8 w-auto" :src="logo" alt="Lexi AI Logo" />
+        </RouterLink>
+
+        <!-- Hamburger menu button -->
+        <button
+          @click="isMenuOpen = !isMenuOpen"
+          class="inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-blue-600 focus:outline-none md:hidden overflow-hidden"
+        >
+          <span class="sr-only">Open main menu</span>
+          <IconMD :path="mdiMenu" class="inline-block" width="28px"/>
+        </button>
+
+
+      <!-- Desktop Menu -->
+      <div class="hidden md:flex space-x-4 items-center">
+          <RouterLink to="/" 
+          :class="[isActiveLink('/') ? 'bg-blue-600' : 'hover:bg-blue-700 hover: text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">Home</RouterLink>
+
+            <span v-if="!isLoggedIn">
+              <RouterLink to="/login" 
+              :class="[isActiveLink('/login') ? 'bg-blue-600' : 'hover:bg-blue-700 hover: text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']">Login</RouterLink>
+            </span>
+            <span v-else  class="flex space-x-4">
+              <button @click="logout"
+              :class="[isActiveLink('/logout') ? 'bg-blue-600' : 'hover:bg-blue-700 hover: text-white', 'block', 'text-white', 'px-3', 'py-2', 'rounded-md']">Logout</button>
+              
+            </span>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile Menu -->
+    <div v-show="isMenuOpen" class="md:hidden bg-blue-500 px-4 space-y-2 pb-4 transition-all">
+      <RouterLink to="/" 
+          :class="[isActiveLink('/') ? 'bg-blue-600' : 'hover:bg-blue-700 hover: text-white', 'block', 'text-white', 'px-3', 'py-2', 'rounded-md']">Home</RouterLink>
+          
+           <span v-if="!isLoggedIn">
+              <RouterLink to="/login" 
+                :class="[isActiveLink('/login') ? 'bg-blue-600' : 'hover:bg-blue-700 hover: text-white', 'block', 'text-white', 'px-3', 'py-2', 'rounded-md']">Login</RouterLink>
+            </span>
+            <span v-else>
+              <button @click="logout"
+              :class="[isActiveLink('/logout') ? 'bg-blue-600' : 'hover:bg-blue-700 hover: text-white', 'block', 'text-white', 'px-3', 'py-2', 'rounded-md']">Logout</button>
+              
+            </span>
+
+    </div>
+  </nav>
+</template>
