e22eefbd80c9510ccffa559c8b9bd98b2c0d1333
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-19 18:07:06 -0500

    login styling

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -100,7 +100,7 @@ const onSubmit = async () => {
             </div>
             <button
               :disabled="isLoading"
-              class="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded disabled:opacity-50"
+              class="w-full lexi-btn"
             >
               <span v-if="isLoading">Loading...</span>
               <span v-else>Login</span>
