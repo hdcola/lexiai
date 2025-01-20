0a7478fb688f7b93955c8f75afd370d1e7e32578
iasssy <iana.setrakova@johnabbottcollege.net>
2025-01-19 22:09:52 -0500

    added validations for Register, fixed previous error showing

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,48 +1,72 @@
 <script setup lang="ts">
 import ImgRegister from '@/components/images/ImgRegister.vue'
+import { useAuthStore } from '@/stores/auth'
+import * as Yup from 'yup'
 import { ref } from 'vue'
-import axios from 'axios'
-import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
 
 const username = ref<string>('')
 const email = ref<string>('')
 const password = ref<string>('')
 const confirmPassword = ref<string>('')
-const errorMessage = ref<string>('')
-const successMessage = ref<string>('')
-const router = useRouter()
+authStore.errorMessage = ''
+authStore.successMessage = ''
 
-const apiUrl = import.meta.env.VITE_API_URL
-const apiPORT = import.meta.env.VITE_API_PORT
+// validation errors
+const errors = ref<Record<string, string>>({})
 
-const register = async () => {
-    errorMessage.value = ''
-    successMessage.value = ''
+const schema = Yup.object({
+    username: Yup.string()
+        .required('Username is required')
+        .min(4, 'Username must be at least 4 characters long')
+        .max(50, 'Username is too long'),
+    email: Yup.string()
+        .email('Invalid email format')
+        .required('Email is required')
+        .max(360, 'Email is too long'),
+    password: Yup.string()
+        .required('Password is required')
+        .min(6, 'Password must be at least 6 characters long'),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password')], 'Password must match')
+        .required('Confirm password is required'),
+})
 
-    if (!username.value || !email.value || !password.value || !confirmPassword.value) {
-        errorMessage.value = 'All fields are required.'
-        return
+const isLoading = ref(false)
+
+// clear error if input changes
+const clearError = (field: string) => {
+    if (errors.value[field]) {
+        delete errors.value[field]
     }
+}
 
-    if (password.value !== confirmPassword.value) {
-        errorMessage.value = 'Passwords do not match.'
-        return
-    }
+const onSubmit = async () => {
+    isLoading.value = true
+    errors.value = {} // Reset errors
 
     try {
-        const response = await axios.post(`${apiUrl}:${apiPORT}/api/users/register`, {
-            username: username.value,
-            email: email.value,
-            password: password.value,
-        })
-
-        console.log('Registration successful:', response.data)
-
-        successMessage.value = 'Registration successful. Please log in.'
-        setTimeout(() => router.push('/login'), 2000) // Redirect after 3 seconds
-    } catch (error) {
-        console.log('Registration error:', error.response?.data?.message || error)
-        errorMessage.value = error.response?.data?.message || 'Registration failed. Please try again.';
+        await schema.validate(
+            {
+                username: username.value,
+                email: email.value,
+                password: password.value,
+                confirmPassword: confirmPassword.value,
+            },
+            { abortEarly: false },
+        )
+        await authStore.register(username.value, email.value, password.value)
+    } catch (validationError) {
+        if (validationError instanceof Yup.ValidationError) {
+            validationError.inner.forEach((err) => {
+                if (err.path && err.message) {
+                    errors.value[err.path] = err.message
+                }
+            })
+        }
+    } finally {
+        isLoading.value = false
     }
 }
 </script>
@@ -59,24 +83,27 @@ const register = async () => {
                 <h2 class="mt-5 text-center text-2xl font-bold text-gray-900">Sign Up</h2>
 
                 <form
-                    @submit.prevent="register"
+                    @submit.prevent="onSubmit"
                     class="space-y-8 mt-10 sm:mx-auto sm:w-full sm:max-w-sm"
                 >
+                    <!-- Success and Error Messages -->
                     <div
-                        v-if="errorMessage"
+                        v-if="authStore.errorMessage"
                         role="alert"
                         class="bg-red-50 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center"
+                        aria-live="polite"
                     >
-                        {{ errorMessage }}
+                        {{ authStore.errorMessage }}
                     </div>
                     <div
-                        v-if="successMessage"
+                        v-if="authStore.successMessage"
                         role="alert"
                         class="bg-green-50 border-green-400 text-green-700 px-4 py-3 rounded relative text-center"
+                        aria-live="polite"
                     >
-                        {{ successMessage }}
+                        {{ authStore.successMessage }}
                     </div>
-                    <div>
+                    <div class="form-group">
                         <label for="username" class="block mb-2 text-sm font-medium text-gray-900"
                             >Username</label
                         >
@@ -85,13 +112,15 @@ const register = async () => {
                             name="username"
                             id="username"
                             v-model="username"
-                            minlength="4"
-                            required
+                            @input="clearError('username')"
                             class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
                         />
+                        <div v-if="errors.username" class="text-red-500 text-sm mt-1">
+                            {{ errors.username }}
+                        </div>
                     </div>
 
-                    <div>
+                    <div class="form-group">
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900"
                             >Email address</label
                         >
@@ -100,12 +129,15 @@ const register = async () => {
                             name="email"
                             id="email"
                             v-model="email"
-                            required
+                            @input="clearError('email')"
                             class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
                         />
+                        <div v-if="errors.email" class="text-red-500 text-sm mt-1">
+                            {{ errors.email }}
+                        </div>
                     </div>
 
-                    <div>
+                    <div class="form-group">
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900"
                             >Password</label
                         >
@@ -114,14 +146,15 @@ const register = async () => {
                             name="password"
                             id="password"
                             v-model="password"
-                            required
-                            minlength="6"
-                            maxlength="50"
+                            @input="clearError('password')"
                             class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
                         />
+                        <div v-if="errors.password" class="text-red-500 text-sm mt-1">
+                            {{ errors.password }}
+                        </div>
                     </div>
 
-                    <div>
+                    <div class="form-group">
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900"
                             >Confirm password</label
                         >
@@ -130,12 +163,18 @@ const register = async () => {
                             name="confirmPassword"
                             id="confirmPassword"
                             v-model="confirmPassword"
-                            required
+                            @input="clearError('confirmPassword')"
                             class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
                         />
+                        <div v-if="errors.confirmPassword" class="text-red-500 text-sm mt-1">
+                            {{ errors.confirmPassword }}
+                        </div>
                     </div>
 
-                    <button type="submit" class="w-full lexi-btn">Register</button>
+                    <button :disabled="isLoading" class="w-full lexi-btn">
+                        <span v-if="isLoading">Loading...</span>
+                        <span v-else>Register</span>
+                    </button>
                 </form>
 
                 <p class="mt-5 text-center text-gray-900">
