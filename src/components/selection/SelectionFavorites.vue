20c26e5fde893cbb8ac00d3fd63103d9404660ee
Tamara Plante <0631064@johnabbottcollege.net>
2025-01-27 18:56:14 -0500

    Fix favorites topic id
    Temp fix

--- src/components/selection/SelectionFavorites.vue
+++ src/components/selection/SelectionFavorites.vue
@@ -4,19 +4,18 @@ import ButtonFavorite from '../ButtonFavorite.vue'
 import IconPlay from '../images/icons/IconPlay.vue'
 import type { ITopic } from '../GeminiSelection.vue'
 import { useUserStore } from '@/stores/user'
-import { useServerRequest } from '@/assets/composables/useServerRequest';
+import { useServerRequest } from '@/assets/composables/useServerRequest'
 
 const emit = defineEmits(['selection'])
 const userStore = useUserStore()
 const favorites = userStore.getFavorites()
-
 
 const topics = ref<ITopic[]>([])
 const selectedTopic = ref<ITopic | null>(null)
 
 async function fetchTopics() {
     try {
-        const topicsResponse = await useServerRequest('get', '/api/users/favorites');
+        const topicsResponse = await useServerRequest('get', '/api/users/favorites')
 
         topics.value = topicsResponse?.data.map(
             (topic: { _id: { $oid: string }; title: string; level: string }) => ({
@@ -52,7 +51,7 @@ function handleFavorite(topic: ITopic) {
     topic.isFavorite = !topic.isFavorite
     topics.value.filter((t) => t._id != topic._id)
     // Update in store & MongoDB
-    userStore.toggleFavorite(topic._id, topic.isFavorite)
+    userStore.toggleFavorite(topic._id.$oid, topic.isFavorite)
 }
 
 async function onActivated() {
@@ -73,7 +72,7 @@ defineExpose({
     <ul class="flex-1 overflow-y-auto px-4">
         <li
             v-for="topic in topics"
-            :key="topic._id"
+            :key="topic._id.$oid"
             class="bg-white p-3 px-4 rounded-lg shadow-md mx-auto mb-4"
         >
             <div class="flex flex-row gap-4 items-center">
