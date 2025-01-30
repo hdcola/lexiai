02564b20ea98d959fa8246e5b5efeff68b92d3ef
Tamara Plante <0631064@johnabbottcollege.net>
2025-01-30 02:34:13 -0500

    Fix settings responsive layout

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -78,7 +78,7 @@ onMounted(() => {
             </div>
         </div>
         <div class="w-full lg:w-1/2 flex justify-center lg:justify-start lg:ps-10 lg:pt-16">
-            <div class="flex flex-col w-full md:w-2/3">
+            <div class="flex flex-col w-full lg:w-4/5">
                 <h2 class="text-center text-2xl font-bold text-gray-900 mb-4">Settings</h2>
 
                 <div class="border-b flex justify-center">
@@ -90,7 +90,7 @@ onMounted(() => {
                         data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300"
                         role="tablist"
                     >
-                        <li class="me-2" role="presentation">
+                        <li class="me-1" role="presentation">
                             <button
                                 id="profile-styled-tab"
                                 data-tabs-target="#styled-profile"
@@ -103,7 +103,7 @@ onMounted(() => {
                                 Profile
                             </button>
                         </li>
-                        <li class="me-2" role="presentation">
+                        <li class="me-1" role="presentation">
                             <button
                                 id="security-styled-tab"
                                 data-tabs-target="#styled-security"
@@ -116,7 +116,7 @@ onMounted(() => {
                                 Security
                             </button>
                         </li>
-                        <li class="me-2" role="presentation">
+                        <li class="me-1" role="presentation">
                             <button
                                 id="dashboard-styled-tab"
                                 data-tabs-target="#styled-dashboard"
