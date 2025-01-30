5e0cfc0e68d30c736fc7a5476997cd4085cf568d
Tamara Plante <0631064@johnabbottcollege.net>
2025-01-30 02:00:55 -0500

    Smooth page transition

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
 import AppNavbar from './components/AppNavbar.vue'
-import { RouterView, useRoute } from 'vue-router'
-
-const route = useRoute()
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-    <div class="background w-full min-h-screen" :class="route.name">
-        <AppNavbar />
-        <RouterView />
-    </div>
+    <RouterView v-slot="{ Component, route }">
+        <div class="background w-full min-h-screen" :class="route.name">
+            <AppNavbar />
+            <Transition name="fade" mode="out-in">
+                <div :key="route.name">
+                    <component :is="Component" />
+                </div>
+            </Transition>
+        </div>
+    </RouterView>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+/* Hide the patterned background until we are done smooth transition */
+.background.home:has(.fade-enter-active),
+.background.home:has(.fade-leave-active) {
+    background: theme(colors.blue.100);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
